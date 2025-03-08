4af97aefb8803b1243ebca5d67598f378d7a7e4e
AlekysCoelho <alekyscoelho@gmail.com>
2025-03-08 12:53:26 -0300

    feat(conftest, factories): Add initial factories and fixtures setup
    Adds `UserFactory` and SuperUserFactory` for creating test users and superusers.
    Adds `new_user` fixture for creating a single user.
    Adds `new_super_user` fixture for creating superuser.
    Registers `UserFactory` `SuperUserFactory` in `conftest.py`.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,30 @@
 import pytest
+from django.conf import settings
+from pytest_factoryboy import register
+
+from tests.factories import SuperUserFactory, UserFactory
+
+register(UserFactory)
+register(SuperUserFactory)
+
+
+@pytest.fixture(scope="session")
+def django_db_setup():
+    settings.DATABASES["default"] = {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "test_django_db",
+        "NAME": "test_djangodb",
+        "PASSWORD": "TestDjangoDB",
+        "USER": "test_django",
+        "PORT": 5432,
+    }
+
+
+@pytest.fixture
+def new_user(db, user_factory):
+    return user_factory()
+
+
+@pytest.fixture
+def new_super_user(db, super_user_factory):
+    return super_user_factory()
--- tests/factories.py
+++ tests/factories.py
@@ -1,3 +1,48 @@
+import logging
+
 import factory
-import faker
+
+# from app.accounts.models import User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 from faker import Faker
+
+from app.logs import get_logger
+
+User = get_user_model()
+faker = Faker()
+
+
+test_logger = get_logger("test", logging.WARNING)
+
+
+class BaseUserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+        abstract = True
+        django_get_or_create = (
+            "username",
+            "email",
+        )
+
+    username = factory.Sequence(lambda n: f"user{n}")
+    email = factory.Faker("email")
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    is_active = True
+
+    @factory.post_generation
+    def password(self, create, extracted, **kwargs):
+        self.password = make_password(extracted or "password123")
+        if create:
+            self.save()
+
+
+class UserFactory(BaseUserFactory):
+    is_staff = False
+    is_superuser = False
+
+
+class SuperUserFactory(BaseUserFactory):
+    is_staff = True
+    is_superuser = True
