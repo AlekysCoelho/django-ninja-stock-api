829304c55d326a6278097d0de99b74793fcd090b
AlekysCoelho <alekyscoelho@gmail.com>
2025-03-11 21:52:48 -0300

    feat(urls): Serve static files in development
    Adds static file serving to `urlpatterns` using `django.conf.urls.static`.
    
    This change allows static files (CSS, JavaScript, images) to be served during development.

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin  # noqa F401
 from django.urls import path
 
+from app.accounts.admin import admin_site
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+    path("admin/", admin_site.urls),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
