a63627a3ba37b55f91ba8ee990471260bc160557
AlekysCoelho <alekyscoelho@gmail.com>
2025-03-24 19:31:38 -0300

    feat(settings): register products app in Django settings
    Add 'app.products' to INSTALLED_APPS to enable:
    - Model registration
    - Database migrations
    - App-specific configurations
    - Test discovery
    
    Changes:
    - Updated INSTALLED_APPS in settings.py
    - Ensures Django recognizes the products app components
    
    This change is required for the products app to be properly
    integrated with the Django framework.

--- app/settings.py
+++ app/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     # Third-party apps
     # Local Apps
     "app.accounts.apps.AccountsConfig",
+    "app.products.apps.ProductsConfig",
 ]
 
 MIDDLEWARE = [
@@ -82,14 +83,6 @@ DATABASES = {
         "PORT": config("POSTGRES_PORT", default=5432, cast=int),
     }
 }
-
-# SQLITE
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
